d9b1638fdf6b2ceb3635f2078fea980edfe26328
Nirbhay <nirbhay0299@gmail.com>
2021-04-07 01:54:29 +0530

    Important Question

--- DynamicProgramming/BuySellTwoTransaction.java
+++ DynamicProgramming/BuySellTwoTransaction.java
@@ -23,7 +23,49 @@ public class BuySellTwoTransaction {
 
     private static int buySellTwoStock(int[] arr) {
         
-        
-        return 0;
+        int leastSoFar = arr[0];
+        int mpist = 0; // max profit if sell today
+        int n = arr.length;
+        int maxProfitIfSellOnOrBeforeToday[] = new int[n];
+        int maxProgitIfBoughOnOrAfterToday[] = new int[n];
+
+        for (int i = 1; i < maxProfitIfSellOnOrBeforeToday.length; i++) {
+            
+
+            if (leastSoFar > arr[i] )
+                leastSoFar = arr[i];
+
+            mpist = arr[i] - leastSoFar;
+
+            if(maxProfitIfSellOnOrBeforeToday[i-1] < mpist){
+                maxProfitIfSellOnOrBeforeToday[i] = mpist;
+            } else {
+                maxProfitIfSellOnOrBeforeToday[i] = maxProfitIfSellOnOrBeforeToday[i-1];
+            }
+        }
+
+        int mpibt = 0; // max profit if bought toady 
+        int maxSoFar = arr[n-1];
+
+        for (int i = n-2; i >= 0; i--) {
+            if (arr[i] > maxSoFar){
+                maxSoFar = arr[i];
+            }
+            mpibt = maxSoFar - arr[i];
+            if (maxProgitIfBoughOnOrAfterToday[i-1] < mpibt){
+                maxProgitIfBoughOnOrAfterToday[i]  = mpibt;
+            } else {
+                maxProgitIfBoughOnOrAfterToday[i] = maxProgitIfBoughOnOrAfterToday[i-1];
+            }
+        }
+
+        int overallProfit = 0;
+
+        for(int i = 0; i < n; i++) {
+            if (overallProfit < maxProfitIfSellOnOrBeforeToday[i] + maxProgitIfBoughOnOrAfterToday[i]){
+                overallProfit = maxProfitIfSellOnOrBeforeToday[i] + maxProgitIfBoughOnOrAfterToday[i];
+            }
+        }
+        return overallProfit;
     }
 }
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This folder contains varoius question's solution that I solved on sites like gee
 
 ## Some Importent Questions 
 
-1. Dp
-    1. [Buy Sell TeoTransactions Allowed](https://github.com/maharana0299/Practice-Questions/blob/master/DynamicProgramming/BuySellTwoTransaction.java).
+1. Dynaic Programming 
+    1. [Buy Sell Two Transactions Allowed](https://github.com/maharana0299/Practice-Questions/blob/master/DynamicProgramming/BuySellTwoTransaction.java).
 
