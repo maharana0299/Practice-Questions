5b647938aa2eb539d24ea9afdd1f2e3eb2c84ecc
Nirbhay <nirbhay0299@gmail.com>
2021-04-17 21:11:35 +0530

    .

---
+++ Mix/Arrays and dp mix/WordSubset.java
@@ -0,0 +1,33 @@
+
+public class WordSubset {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        
+        int count[] = count("");
+        
+        for(String b : B) {
+            int[] bCount = count(b);
+            for(int i = 0; i < 26; i++) {
+                count[i] = Math.max(count[i],bCount[i]);
+            }
+        }
+        
+        List<String> ans = new ArrayList<>();
+        
+        search: for(String a : A) {
+            int[] aCount = count(a);
+            for(int i = 0; i < 26; i++)
+                if(aCount[i] < count[i])
+                    continue search; //if count is less then skip 
+            ans.add(a);
+        }
+        
+        return ans;
+    }
+    
+    public int[] count(String S) {
+        int[] ans = new int[26];
+        for (char c: S.toCharArray())
+            ans[c - 'a']++;
+        return ans;
+    }    
+}
