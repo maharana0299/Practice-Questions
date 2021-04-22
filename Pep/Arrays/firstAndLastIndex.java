50681390d57ae9182edb0757c0f491f952b69f77
Nirbhay <nirbhay0299@gmail.com>
2021-04-22 21:55:46 +0530

    .

--- Pep/Arrays/firstAndLastIndex.java
+++ Pep/Arrays/firstAndLastIndex.java
@@ -36,4 +36,45 @@ public class firstAndLastIndex {
         System.out.println(last);
     }
 
+    public int firstIndex(int arr[], int data){
+        int fi = -1;
+        int lo = 0;
+        int hi = arr.length;
+
+        while(lo<=hi){
+
+            int mid = (lo+hi)/2;
+
+            if(arr[mid] == data){
+                fi = mid;
+                hi = mid - 1;
+            } else if(arr[mid] < data){
+                lo = mid + 1;
+            } else {
+                hi = mid-1;
+            }
+        }
+        return fi;
+    }
+
+    public int lastIndex(int arr[], int data){
+        int li = -1;
+        int lo = 0;
+        int hi = arr.length;
+
+        while(lo<=hi){
+
+            int mid = (lo+hi)/2;
+
+            if(arr[mid] == data){
+                li = mid;
+                lo = mid + 1;
+            } else if(arr[mid] < data){
+                lo = mid + 1;
+            } else {
+                hi = mid-1;
+            }
+        }
+        return li;
+    }
 }
