9a06b00196ce5bac3ac3b4df9311ff7c54185af5
Nirbhay <nirbhay0299@gmail.com>
2021-04-27 20:09:10 +0530

    .

--- Web Development/Lec7/facts/htmlRead.js
+++ Web Development/Lec7/facts/htmlRead.js
@@ -6,8 +6,26 @@ let htmlKaData = fs.readFileSync("./index.html");
 let ch = cheerio.load(htmlKaData);
 
 // <h1>Heading 1</h1> => object form !!
-let liKaObject = ch('li');
-// console.log(liKaObject);
+let liKaObject = ch('tr');
+// console.log(liKaObject.length);
 
-let h1KaData = ch("li").text();
-console.log(h1KaData);
+let ps = ch('p');
+lastP = ch(ps[2]);
+// console.log(lastP.text());
+
+let pInsideLi = ch('ul li p'); // selectors 
+// console.log(pInsideLi.text());
+
+let pisideUl = ch('ul>p'); // > this is for direct childre;
+// console.log(pisideUl.text());
+// let h1KaData = ch("li").text();
+// console.log(h1KaData)
+
+// class -multiple - use . for class
+// id- unique - use #
+
+let pClass = ch('.para.main').text(); // no space for same element
+// console.log(pClass);
+
+let pId = ch('#unique');
+console.log(pId.text());
