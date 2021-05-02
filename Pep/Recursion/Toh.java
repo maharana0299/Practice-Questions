ffa02461ef17cf18d42d416d5c2e2950d6be8228
Nirbhay <nirbhay0299@gmail.com>
2021-05-02 20:16:24 +0530

    Toh done

---
+++ Pep/Recursion/Toh.java
@@ -0,0 +1,31 @@
+/**
+ * toh
+ */
+public class Toh {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t1id = sc.nextInt();
+        int t2id = sc.nextInt();
+        int t3id = sc.nextInt();
+        
+        toh(n,t1id,t2id,t3id);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+        
+        if(n == 1){
+            
+            System.out.println(n+"["+t1id+" -> "+t2id+"]");
+            return;
+        }
+        
+        toh(n-1,t1id,t3id,t2id);
+        System.out.println(n+"["+t1id+" -> "+t2id+"]");
+        toh(n-1,t3id,t2id,t1id);
+        // System.out.println(n+"["+t1id+"->"+t3id+"]");
+        
+    }
+}
