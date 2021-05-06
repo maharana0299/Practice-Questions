a27d7d151546ba19f907581e4e31061f3f5a7dda
nirbhay0299@gmail.com <nirbhay0299@gmail.com>
2021-05-06 18:53:46 +0530

    Recursion

---
+++ Pep/ArrayAndRecursion/ArrrayDisp.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import jdk.internal.jshell.tool.resources.l10n;
+
+/**
+ * ArrrayDisp
+ */
+public class ArrrayDisp {
+    
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        displayArr(arr, 0);
+    }
+
+    public static void displayArr(int[] arr, int idx){
+
+        if(idx == arr.length)
+            return;
+        System.out.println(arr[idx]);
+
+        displayArr(arr, idx+1);
+    }
+
+    public static void displayArrRev(int[] arr, int idx){
+
+        if(idx == -1)
+            return;
+        System.out.println(arr[idx]);
+
+        displayArr(arr, idx-1);
+    }
+}
