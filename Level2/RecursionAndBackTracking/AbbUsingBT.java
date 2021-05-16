1c87dd87470ef278d4cca8f5b065956cf6ab7382
Nirbhay <nirbhay0299@gmail.com>
2021-05-16 10:47:34 +0530

    .

---
+++ Level2/RecursionAndBackTracking/AbbUsingBT.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * AbbUsingBT
+ */
+public class AbbUsingBT {
+
+    public static void solution(String str, String asf,int count, int pos){
+        
+        if (pos == str.length()) {
+            if(count == 0)
+            System.out.println(asf);
+            else System.out.println(asf + count);
+            return;
+        }
+        String toPass = asf;
+        // case of 0
+        if(count != 0)
+            toPass += count + "";            
+        solution(str, toPass+str.charAt(pos), 0, pos+1);
+        //case of 1
+        solution(str, asf, count + 1, pos + 1);
+    }
+	public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        solution(str,"",0,0);
+        scn.close();
+    }
+}
---
+++ Level2/RecursionAndBackTracking/NQueenBranchAndBound.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NQueenBranchAndBound {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[][] board = new boolean[n][n];
+        // nQueenBound(n,board);
+        sc.close();
+    }
+}
