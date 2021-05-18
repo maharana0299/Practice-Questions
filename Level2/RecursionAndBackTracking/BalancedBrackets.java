df52d3d08f5b75935377fb7f3f19fdfaa7795a05
Nirbhay <nirbhay0299@gmail.com>
2021-05-19 02:07:52 +0530

    ,

--- Level2/RecursionAndBackTracking/BalancedBrackets.java
+++ Level2/RecursionAndBackTracking/BalancedBrackets.java
@@ -57,5 +57,21 @@ public class BalancedBrackets {
 		solution(str, getMin(str),new HashSet<>());
         scn.close();
 	}
+
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }   
 		
 }
