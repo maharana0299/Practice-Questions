f9443e304a8e5a4efd498daa60a3ffe1de0cae0e
Nirbhay <nirbhay0299@gmail.com>
2021-05-28 00:48:48 +0530

    .

--- Level2/BitsManipulation/CopyInRange.java
+++ Level2/BitsManipulation/CopyInRange.java
@@ -9,12 +9,20 @@ public class CopyInRange {
     int left = scn.nextInt();
     int right = scn.nextInt();
 
+    // creating mask 
+    // shifting the one to gap length away 
     int m = 1 << (right - left + 1);
+
+    // to get all ones of size gap, subtracting by one 
     m = m - 1;
+
+    // shifting by left to get ones in range of left - > right
     m = m << (left - 1);
 
+    // taking and and copying bits in range
     m = a & m;
 
+    // setting the set bits in a to b in range of left ->  rigth
     m = b | m;
 
     System.out.println(m);
