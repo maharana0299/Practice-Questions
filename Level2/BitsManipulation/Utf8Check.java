fbaee98af2aa42954627ef30d06a4d793e1e4f46
nirbhay0299@gmail.com <nirbhay0299@gmail.com>
2021-05-28 20:44:23 +0530

    ,

--- Level2/BitsManipulation/Utf8Check.java
+++ Level2/BitsManipulation/Utf8Check.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
  */
 public class Utf8Check {
 
+    /**
+     * max size of an UTF can be 4
+     * If its size is 1, then it starts with 0
+     * It its size is 2, then it statrs with 110
+     * For size 3, 1110 etc.
+     * For size n, n 1's followd by a 0. Followed by n-1 bits starting with 10 
+     * @param arr
+     * @return
+     */
+
     public static boolean solution(int[] arr) {
         
 
@@ -13,13 +23,16 @@ public class Utf8Check {
         for(int b : arr) {
             
             if(rem == 0) {
+
+                // checking for UTF of size 1
                 if((b>>7) == 0b0) {
                     rem = 0;
-                } else if((b>>5)== 0b110){
+                } else if((b>>5)== 0b110){  // checking for UTF of size 2
                     rem = 1;
-                } else if((b>>4)== 0b1110){
+                } else if((b>>4)== 0b1110){  // checking for UTF of size 3
                     rem = 2;
-                } else if((b>>3) == 0b11110){
+                } else if((b>>3) == 0b11110){ // checking for UTF of size 4
+                    // remaining bits are n-1, ie 3
                     rem = 3;
                 } else {
                     return false;
