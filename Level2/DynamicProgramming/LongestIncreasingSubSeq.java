0420a2171f6048bcb8c51b47f7b16b796b2d4a7a
Nirbhay <nirbhay0299@gmail.com>
2021-05-29 16:17:09 +0530

    .

---
+++ Level2/DynamicProgramming/LongestIncreasingSubSeq.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class LongestIncreasingSubSeq {
+
+    public static void main(String[] args) throws Exception {
+        
+        // taking input 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+            arr[i] = sc.nextInt();
+        
+        int liss = liss(arr, n);
+        System.out.println(liss); 
+    }
+
+    // longest increasing subsequence
+    public static int liss(int[] arr, int n) {
+
+        // overall max
+        int omax = 0;
+        // longest increasing subsequence till ith element including the ith element
+        // to calculate the longest subsequqnce we have to look for every possible value of i
+        // and store the longest subsequence including ith element 
+        // in the end we will return the biggest subsequence 
+        int[] dp = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            
+            int max = 0;
+            
+            for(int j = 0; j < i; j++) {
+                // if less then it can definetly form a subsequence with our i 
+                // so if max is less than dp[j], ie. lcs including j till j
+                if(arr[j] < arr[i]) {
+                    if(dp[j] > max) {
+                        max = dp[j]; // longest subsquence till j 
+                    }
+                }
+            }
+            
+            // after this including i also 
+            dp[i] = max + 1; // longest subsequence till j and then including i also
+            
+            if(dp[i] > omax){
+                omax = dp[i];
+            }
+        }
+        
+        // System.out.println(omax);
+        return omax;
+    }
+
+}
---
+++ Web Development/automation/meet.js
@@ -0,0 +1,113 @@
+const puppeteer = require('puppeteer-extra')
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+function start({ metting_id }) {
+
+    let pages;
+
+    // puppeteer usage as normal
+    puppeteer.launch({
+            headless: false,
+            args: ["--disable-notifications", "--mute-audio", "--enable-automation", '--start-maximized'],
+            ignoreDefaultArgs: true
+        }).then(async browser => {
+            const page = await browser.newPage();
+            const navigationPromise = page.waitForNavigation();
+            await page.goto("https://accounts.google.com/");
+
+            const context = browser.defaultBrowserContext();
+            await context.overridePermissions(
+                "https://meet.google.com/", ["microphone", "camera", "notifications"]
+            );
+
+            await navigationPromise;
+
+            await page.setDefaultTimeout(3600000);
+
+            // await page.setDefaultNavigationTimeout(0); // disable timeout error 
+
+            // typing out email
+            await page.waitForSelector('input[type="email"]');
+            await page.click('input[type="email"]');
+            await navigationPromise;
+
+            await page.keyboard.type(`nk02061999@gmail.com`, {
+                delay: 300
+            }); // rep lace XXXXX with your original email, before the @gmail.com
+            await page.waitForTimeout(2000);
+
+            await page.waitForSelector("#identifierNext");
+            await page.click("#identifierNext");
+
+            // typing out password
+            await page.waitForTimeout(3500);
+            await page.keyboard.type(`IamMahaRana`, { delay: 200 }); // replace YYYYY with your original password
+            await page.waitForTimeout(800);
+            await page.keyboard.press('Enter');
+            await navigationPromise;
+
+            // going to Meet after signing in
+            await page.waitForTimeout(2500);
+            await page.goto("https://meet.google.com/");
+            await page.waitForSelector('input[type="text"]');
+            await page.click('input[type="text"]');
+            await page.waitForTimeout(1000);
+
+            await page.keyboard.type(`${metting_id}`, { delay: 200 }); // replace aaa-bbbb-ccc with the required Google Meet Code
+            await page.waitForTimeout(800);
+            await page.keyboard.press('Enter');
+            await navigationPromise;
+            // turn off cam using Ctrl+E
+            await page.waitForTimeout(8000);
+            await page.keyboard.down('ControlLeft');
+            await page.keyboard.press('KeyE');
+            await page.keyboard.up('ControlLeft');
+            await page.waitForTimeout(2000);
+
+            //turn off mic using Ctrl+D
+            await page.waitForTimeout(1000);
+            await page.keyboard.down('ControlLeft');
+            await page.keyboard.press('KeyD');
+            await page.keyboard.up('ControlLeft');
+            await page.waitForTimeout(2000);
+
+            // Join Now
+            var i;
+            for (i = 1; i <= 9; i++) {
+                await page.keyboard.press('Tab');
+                await page.waitForTimeout();
+            }
+
+            pages = page;
+            // await
+            // await;
+            return Promise.all([page.keyboard.press('Enter'), navigationPromise]);
+        })
+        .then(() => {
+            // open chat section and send a message to all
+            await pages.waitForTimeout();
+
+            // await page.waitForSelector('div[aria-hidden="true"]');
+            for (i = 1; i <= 11; i++) {
+                await pages.keyboard.press('Tab');
+                await pages.waitForTimeout();
+            }
+            await pages.keyboard.press('Enter');
+            await pages.waitForSelector('a[href]');
+            // console.log("Got!!");
+            await pages.waitForTimeout();
+            await pages.click('a[href]');
+            await pages.waitForNavigation();
+            // await browser.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+// wfp-aavb-vma
+// console.log(process.argv[2]);
+start({ metting_id: process.argv[2] });
