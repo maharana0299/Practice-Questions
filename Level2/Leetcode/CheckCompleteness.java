cecbc49c54398c7f1a607b69cd3790d9559a2280
Nirbhay <nirbhay0299@gmail.com>
2021-06-15 12:28:51 +0530

    .

--- Level2/Leetcode/CheckCompleteness.java
+++ Level2/Leetcode/CheckCompleteness.java
@@ -3,49 +3,48 @@ import java.util.Queue;
 
 public class CheckCompleteness {
     /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+     * Definition for a binary tree node. 
+     * public class TreeNode { 
+     *      int val; 
+     *      TreeNode left; 
+     *      TreeNode right; 
+     *      
+     *      TreeNode() {} TreeNode(int val) { this.val = val; }
+     *      TreeNode(int val, TreeNode left, TreeNode right) { 
+     *          this.val = val; 
+     *          this.left = left; 
+     *          this.right = right; 
+     *      } 
+     * }
+     */
     public boolean isCompleteTree(TreeNode root) {
-        
+
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         boolean isNull = false;
-        
-        while(!q.isEmpty()){
-            
+
+        while (!q.isEmpty()) {
+
             int n = q.size();
-            
-            for(int i = 0; i < n; i++) {
+
+            for (int i = 0; i < n; i++) {
                 TreeNode node = q.poll();
-            if( node.left != null ){
-                if(isNull)   return false; 
-                q.add(node.left);
+                if (node.left != null) {
+                    if (isNull)
+                        return false;
+                    q.add(node.left);
+                } else
+                    isNull = true;
+
+                if (node.right != null) {
+                    if (isNull)
+                        return false;
+                    q.add(node.right);
+                } else
+                    isNull = true;
             }
-            else
-                isNull = true;
-                
-            if( node.right != null ){
-                if(isNull)   return false;
-                q.add(node.right);
-            }
-            else
-                isNull = true;
-            }
-        }        
-        
+        }
+
         return isNull;
     }
 }
-
